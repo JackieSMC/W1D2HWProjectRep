022b352482bdce500b61f98c624ff641a75d9a76
Jackie Amador <jamador01@saintmarys.edu>
2017-06-07 12:37:38 -0400

    Added promote button and CSS styling

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const listForm = document.querySelector('#listForm')
-let x = 0
+
 let listArray = []
 
 
 function renderItem(item) {
     const newItem = document.createElement('li')
+
+    const noSpace = item.replace(/\s/g, '');
+
     newItem.innerHTML = `${item}`
+   
+    const promoteButton = document.createElement('button')
+    promoteButton.setAttribute('type', 'button')
+    promoteButton.setAttribute(`id`, `${noSpace}`)
+    promoteButton.textContent = 'Promote'
+    newItem.appendChild(promoteButton)
+
+    promoteButton.addEventListener('click', handlePromote)
 
     return newItem
 }
@@ -19,24 +30,47 @@ function renderList(item) {
     return list
 }
 
+    function validateNewItem(value) {
+        for (let i = 0; i < listArray.length; i++) {
+            if (listArray[i] == value) {
+                alert('Sorry! This list needs a different item! - Try Again')
+                return false
+            }
+        }
+        return true
+    }
+
 
 function handleSubmit(ev) {
     ev.preventDefault()
-    
     const listItem = ev.target
+
+    //Check for if item in list
+    if (validateNewItem(listItem.formListItem.value) == false) {
+        return false
+    }
     
-    if(x == 1) {
+    if(listArray.length > 0) {
     const k = document.querySelector('ul')
     k.remove() 
     }
 
     listArray.unshift(listItem.formListItem.value)
-
     listForm.appendChild(renderList(listItem.formListItem.value))
-
-    //itemForm.appendChild(renderItem(listItem.groceryItem.value))
     listItem.reset()
-    x = 1
+
+}
+function handlePromote(ev) {
+    ev.preventDefault()
+
+    const background = document.querySelector(`#${e.target.id}`)
+    const parent = background.parentElement
+
+    if (parent.style.backgroundColor == 'lightblue') {
+        parent.style.backgroundColor = 'whitesmoke'}
+    else {
+        parent.style.backgroundColor = 'lightblue'
+    }
 }
 
 listForm.addEventListener('submit', handleSubmit)
